78a7c4af2b0a9797796169caf926cc91c7d616cc
Tabish Raza <108555503+webstwk@users.noreply.github.com>
2022-09-25 03:09:26 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -4,29 +4,39 @@ public class Main
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter your 1st num");
-		int input1=sc.nextInt();
+		double input1=sc.nextDouble();
+		
 		System.out.println("enter your 2nd number");
-		int input2=sc.nextInt();
-		System.out.println("press + for addition");
-		System.out.println("press - for subtraction");
-		System.out.println("press * for multiplication");
+		double input2=sc.nextDouble();
+		
+	    	System.out.println("press + for addition");
+	    	System.out.println("press - for subtraction");
+	    	System.out.println("press * for multiplication");
 		System.out.println("press / for division");
+		
+		double addition= input1+input2;
+		double subtraction= input1-input2;
+		double multiplication= input1*input2;
+		double division= input1/input2;
+		
+		
 		char operator = sc.next().charAt(0);
 		
 		switch(operator)
 		{
 		    case '+':
-		        System.out.println(input1+"+"+input2+"="+ (input1+input2));
+		        System.out.println(input1+"+"+input2+"="+addition);
 		        break;
 		    case '-':
-		        System.out.println(input1+"-"+input2+"="+ (input1-input2));
+		        System.out.println(input1+"-"+input2+"="+ subtraction);
 		        break;
 		    case '*':
-		        System.out.println(input1+"*"+input2+"="+ (input1*input2));
+		        System.out.println(input1+"*"+input2+"="+ multiplication);
 		        break;
 		    case '/':
-		        System.out.println(input1+"/"+input2+"="+ (input1/input2));
+		        System.out.println(input1+"/"+input2+"="+ division);
 		        break;
+		   
 		}
 	}
 }
